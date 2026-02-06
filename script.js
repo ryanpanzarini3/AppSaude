3c2c60234686682568fba208773daffc16e1ed3b
Ryan Panzarini Paes <dev.panzarini@gmail.com>
2026-02-06 10:24:13 -0300

    Refactor script.js for improved readability

--- script.js
+++ script.js
@@ -11,6 +11,8 @@ document.addEventListener('DOMContentLoaded', function() {
 });
 
 
+
+
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
         navigator.serviceWorker.register('/sw.js').then(registration => {
